6729c2fb09e90678c41be4b2e8ef05807f4a3825
tetsaiga86 <tetsaiga86@gmail.com>
2016-05-25 15:39:50 -0700

    fixing heroku

--- js/project.js
+++ js/project.js
@@ -2,13 +2,26 @@
   context.projects = [];
 
   context.retrieveProjectHistory = function(render){
-    repos.requestRepos = function(callback) {
-      $.get('/github/users/tetsaiga86/repos' +
-      '?per+page=15' +
-      '&sort=updated')
-      .done(function(data){
-        repos.all = data;
-      }).done(callback);
-    };
+    $.get('/github/users/codefellows-seattle-301d4/repos' +
+    '?per+page=15' +
+    '&sort=updated')
+    .done(function(data){
+      context.projects = data.filter(function(project){
+        return !project.fork;
+      });
+    }).done(render);
   };
+    // $.ajax({
+    //   url: 'https://api.github.com/users/tetsaiga86/repos',
+    //   type: 'GET',
+    //   headers: {'Authorization': 'token ' + githubkey},
+    //   success: function(data){
+    //     localStorage.projectHistory = JSON.stringify(data);
+    //     context.projects = data.filter(function(project){
+    //       return !project.fork;
+    //     });
+    //     render();
+    //   }
+    // });
+  // };
 })(window);
